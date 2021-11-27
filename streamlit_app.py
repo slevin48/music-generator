6d3a40d4ebdc0ae89960dbca0d1864cf68d5b00c
Slevin <yann.debray@gadz.org>
2021-11-27 12:40:38 +0100

    Add search feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,20 +17,40 @@ except OSError as error:
 
 st.title('Music Generator📻')
 
-url = 'https://www.youtube.com/watch?v=6no3uMeMIr8'
 
-url = st.text_input('Youtube URL',url)
+# 2 modes: youtube URL or search
+src = st.radio("Youtube URL or Search",["Youtube URL","Youtube Search"])
 
-# Download Youtube video
-youtube = pytube.YouTube(url)
-videos = youtube.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')
-# l = [s.resolution + " (" + str(round(s.filesize/2**10/2**10)) + " MB)" for s in videos]
-# i = st.radio("Select resolution",range(len(videos)),format_func = lambda x: l[x])
-# video = videos[i]
-video = videos.first()
+
+
+if src == "Youtube URL":
+
+    # Indie/Pop/Folk Compilation - December 2021
+    # url = 'https://www.youtube.com/watch?v=6no3uMeMIr8'
+    # One Repubilc - Someday
+    url = "https://www.youtube.com/watch?v=vNfgVjZF8_4" 
+    url = st.text_input('Youtube URL',url)
+    # Download Youtube video
+    youtube = pytube.YouTube(url)
+    videos = youtube.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')
+    # l = [s.resolution + " (" + str(round(s.filesize/2**10/2**10)) + " MB)" for s in videos]
+    # i = st.radio("Select resolution",range(len(videos)),format_func = lambda x: l[x])
+    # video = videos[i]
+    video = videos.first() 
+    
+else:
+
+    search = st.text_input('Youtube Search',"OneRepublic - Someday")
+    r = pytube.Search(search).results
+    # st.write(r[0].streams.first().title)
+    # t = [i.streams.first().title for i in r]
+    # st.radio("Search results",t)
+    video = r[0].streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+
 dl = st.button('Download')
 
-if dl:   
+if dl:
+
     st.write(video)
     title = video.title
     path = video.download('downloads')
@@ -46,6 +66,8 @@ if dl:
         my_clip.audio.write_audiofile("downloads/music.mp3")
         st.text("Duration: "+format_time(duration))
         st.audio("downloads/music.mp3", format='audio/mp3')
+        with open("downloads/music.mp3", "rb") as file:
+            st.download_button("Download music",data=file,file_name=title+".mp3")
 
     except NameError:
         print('No video to process')
